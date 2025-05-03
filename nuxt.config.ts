17aebdf602ea342ba22dc9ec57a873dad71687a1
soun Soratha <121934976+ZensYing@users.noreply.github.com>
2025-05-03 23:55:46 +0700

    [modify]  unuse file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,9 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     databaseUrl: '',
+    public: {
+      url: '',
+    },
     google: {
       clientId: '',
       clientSecret: '',
@@ -19,18 +22,9 @@ export default defineNuxtConfig({
       secretKey: '',
       webhookSecret: '',
     },
-    bakongSecretKey: process.env.BAKONG_SECRET_KEY,
-
-    // Public keys that are exposed to the client
-    public: {
-      url: '',
-      bakongApiEndpoint: process.env.BAKONG_API_ENDPOINT || 'https://api.bakong.nbc.gov.kh/v1/payments',
-      bakongMerchantId: process.env.BAKONG_MERCHANT_ID || 'demo-merchant',
-      bakongToken: process.env.BAKONG_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiYWIwMGZkODVkMzYzNDFmNiJ9LCJpYXQiOjE3NDYyODk2MzQsImV4cCI6MTc1NDA2NTYzNH0.4rECPlYDDLQH5GltdhC1VC8Jql_TiZuGoj508CWEjOo',
-      bakongTokenExpiry: process.env.BAKONG_TOKEN_EXPIRY || '2025-08-01',
-    },
   },
   modules: ['@nuxt/ui', '@formkit/auto-animate/nuxt', '@nuxtjs/plausible'],
+
   plausible: {
     domain: process.env.PLAUSIBLE_DOMAIN,
     apiHost: process.env.PLAUSIBLE_API_HOST ?? 'https://plausible.io',
